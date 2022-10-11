47ca9d99d6570902560686c02b7b18765bc85832
Davibgentil <davi.gentil@hotmail.com>
2022-10-11 16:06:10 -0300

    Grupo 13 Requisito 21 Davi Gentil

---
+++ src/tests/helpers/Game.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { renderWithRouterAndRedux } from './renderWithRouterAndRedux';
+
+import Game from '../../pages/Game';
+test.only
+describe('Testando a página Game', () => {
+  test('Deve possuir a categoria da questão', async () => {
+    renderWithRouterAndRedux(<Game />);
+
+    const category = await screen.getByTestId('question-category')
+    expect(category).toBeInTheDocument()
+  });
+  test('Deve possuir o texto da questão', async () => {
+    renderWithRouterAndRedux(<Game />);
+
+    const text = await screen.getByTestId('question-text')
+    expect(text).toBeInTheDocument()
+  });
+  test('Deve possuir as alternativas da questão', async () => {
+    renderWithRouterAndRedux(<Game />);
+
+    const ansewerOptions = await screen.getByTestId('answer-options')
+    expect(ansewerOptions).toBeInTheDocument()
+  });
+  test('Deve mostrar a imagem do jogador', async () => {
+    renderWithRouterAndRedux(<Game />);
+
+    const img = await screen.getByTestId('header-profile-picture')
+    expect(img).toBeInTheDocument()
+  });
+  test('Deve mostrar o nome do jogador', async () => {
+    renderWithRouterAndRedux(<Game />);
+
+    const name = await screen.getByTestId('header-player-name')
+    expect(name).toBeInTheDocument()
+  });
+  test('Deve mostrar o score do jogador', async () => {
+    renderWithRouterAndRedux(<Game />);
+
+    const score = await screen.getByTestId('header-score')
+    expect(score).toBeInTheDocument()
+  });
+});
